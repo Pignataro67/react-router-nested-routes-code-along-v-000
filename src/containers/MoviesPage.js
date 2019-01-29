824a87d8bdb32c56a8207180f159865792f78568
Pat  Pignataro <pat6284@msn.com>
2019-01-29 00:31:55 +0000

    Automatically backed up by Learn

--- src/containers/MoviesPage.js
+++ src/containers/MoviesPage.js
@@ -1,18 +1,20 @@
-// .src/containers/MoviesPage.js
-import React from 'react';
-import { Route } from 'react-router-dom';
-import MoviesList from '../components/MoviesList';
-import MovieShow from './MovieShow';
+// // .src/containers/MoviesPage.js
+// import React from 'react';
+// import { Route } from 'react-router-dom';
+// import MoviesList from '../components/MoviesList';
+// import MovieShow from './MovieShow';
 
-const MoviesPage = ({ match, movies }) => (
-  <div>
-    <MoviesList movies={movies} />
-     <Route exact path={match.url} render={() => (
-      <h3>Please select a Movie from the list.</h3>
-    )}/>
-    <Route path={`${match.url}/:movieId`} component={MovieShow}/>
-  </div>;
+// const MoviesPage = ({ match, movies }) => (
+//   <div>
+//     <MoviesList movies={movies} />
+//     <Route exact path={match.url} render={() => (
+//       <h3>Please select a Movie from the list.</h3>
+//     )}/>
+//     <Route path={`${match.url}/:movieId`} component={MovieShow}/>
+//   </div>;
 
-)
+// )
 
-export default MoviesPage
+// export default MoviesPage
+
+
