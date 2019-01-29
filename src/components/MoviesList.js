a4c2bc7648abf3586dff0d04c0def28064b1c95a
Pat  Pignataro <pat6284@msn.com>
2019-01-29 00:33:16 +0000

    Automatically backed up by Learn

--- src/components/MoviesList.js
+++ src/components/MoviesList.js
@@ -1,21 +1,3 @@
-// import React from 'react';
-// import { Link } from 'react-router-dom';
-
-// const moviesList = ({ movies }) => {
-//   Object.keys(movies).map(movieID => <Link key={movieID} to={`/movies/${movieID}`}>
-//   {movies[movieID].title}
-//   </Link>
-//   );
- 
-//   return (
-//     <div>
-//       {renderMovies}
-//     </div>
-//   );
-// };
- 
-// export default MoviesList;
-
 import React from 'react';
 import { Link } from 'react-router-dom';
  
